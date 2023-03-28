ead94ae1037f3dc787d72efb4527f53994e06e9e
Rado <rpflis@gmail.com>
2023-03-28 20:50:58 +0200

    cardlist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello.js';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Hello greeting={'Hello' + 'React Ninja'} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<CardList robots={robots} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
