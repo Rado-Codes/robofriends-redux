6166096b5f9603e3a77c838329ebd9917d616f93
Radoslaw <125380891+Rado-Codes@users.noreply.github.com>
2023-05-04 11:43:12 +0200

    introduced hooks useState and useEffect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
@@ -7,44 +7,37 @@ import './App.css'
 
 
 //Create component
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    
     //Create function for event of SearchField Component
-    onSearchField = (event) => {
-        this.setState({searchField: event.target.value});
-    }
-    //Connect Database with App and update when DOM tree is loaded
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=> response.json())
-        .then(users=> this.setState({robots: users}));
-    }
+    const onSearchField = (event) => {
+        setSearchField(event.target.value);
+    };
 
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => setRobots(users));
+    },[]); //when we wanna run the useEffect  in the second argument
+
+    const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
 
     //render component
-    render() {
-        const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
-        return !robots.length ? //ternary operator if no dta from database then Loading
+    return !robots.length ?
         <h1>Loading</h1> :
         (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchField}/>
+                <SearchBox searchChange={onSearchField}/>
                 <Scroll>
                     <ErrorBoundary>
                         <CardList robots={filteredRobots}/>
                     </ErrorBoundary>
                 </Scroll>
             </div>
-        );
-    }
+    );
 }
 
 export default App;
